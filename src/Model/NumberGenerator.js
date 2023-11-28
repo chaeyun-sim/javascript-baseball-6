932d755a74d538e2f43fafed618b4e4b5c33bd60
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-28 14:56:13 +0900

    ♻️ refactor: NumberGenerator 클래스 수정

---
+++ src/Model/NumberGenerator.js
@@ -0,0 +1,31 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+class NumberGenerator {
+  #computer;
+  constructor() {
+    this.#computer = 0;
+    this.#generate();
+  }
+
+  #generate() {
+    const computer = [];
+
+    while (computer.length < 3) {
+      const RANDOM_NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(RANDOM_NUMBER)) {
+        computer.push(RANDOM_NUMBER);
+      }
+    }
+
+    this.#computer = computer.join(''));
+  }
+
+  /**
+   * @returns {number}
+   **/
+  returnValue() {
+    return this.#computer;
+  }
+}
+
+export default NumberGenerator;
