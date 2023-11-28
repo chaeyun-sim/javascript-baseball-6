9b7b02f4fe8c23ca894c77f5c5c9d2559b23f2b0
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-28 13:51:13 +0900

    ✨ feat: 게임 시작 및 사용자 입력 요청 기능 구현

--- src/Controller/Controller.js
+++ src/Controller/Controller.js
@@ -1,13 +1,28 @@
 import NumberGenerator from '../Model/NumberGenerator';
+import OutputView from '../View/OutpuView';
+import InputView from '../View/InputView';
 
 class Controller {
   #computer;
   constructor() {
     this.#computer = null;
+    this.play();
   }
 
-  startGame() {
+  async play() {
+    this.requestGenerateNumber();
+    await this.startGame();
+  }
+
+  requestGenerateNumber() {
     this.#computer = new NumberGenerator().returnValue();
+  }
+
+  async startGame() {
+    OutputView.printGameStart();
+
+    const INPUT = await InputView.readNumber();
+    return INPUT;
   }
 }
 
